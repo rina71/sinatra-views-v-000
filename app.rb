7ef490aca7fe6949121bb139537d0969150548f8
Yurina Shin <yurinas@gmail.com>
2018-08-12 17:23:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,14 +2,14 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-	get '/' do
-		"<h1>Hello World</h>"
-	end
+	# get '/' do
+	# 	"<h1>Hello World</h>"
+	# end
 	get '/' do
       erb :index
     end
 		get "/info" do
       "Testing the info page"
     end
-		
+
 end
